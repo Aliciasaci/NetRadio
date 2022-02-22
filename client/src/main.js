24a902ceeb4b5d9fb6572c33ec01c6bbfa68298b
Tania Olivia <taniaolivia9@gmail.com>
2022-02-22 22:47:37 +0100

    ajout v-calender vuejs

--- client/src/main.js
+++ client/src/main.js
@@ -4,16 +4,32 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VCalendar from 'v-calendar';
+
+
 
 Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false
+
+Vue.use(VCalendar, {
+    formats: {
+      title: "MMMM YYYY",
+      weekdays: "W",
+      navMonths: "MMM",
+      input: ["L", "YYYY-MM-DD", "YYYY/MM/DD"],
+      dayPopover: "L"
+    }
+  });
 
 Vue.component("HeaderPrincipal", () =>
     import ("@/components/HeaderPrincipal.vue"));
 
 Vue.component("HeaderAnimateur", () =>
     import ("@/components/HeaderAnimateur.vue"));
+
+Vue.component("HeaderAdministrateur", () =>
+    import ("@/components/HeaderAdministrateur.vue"));
 
 Vue.component("Footer", () =>
     import ("@/components/Footer.vue"));
